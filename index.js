74150677b9cdafebc525fd059808076d9de59d89
Alexis Dela Torre <alexisndelatorre@gmail.com>
2022-01-11 09:29:50 +0000

    Successfully connected to MongoDB

--- index.js
+++ index.js
@@ -1,73 +1,88 @@
 const express = require ('express');
+const cors = require('cors');
+require('dotenv').config()
 const hbs = require('hbs');
 const wax = require('wax-on');
+const axios = require('axios');
+const MongoUtil = require('./MongoUtil.js');
+const ObjectId = require('mongodb').ObjectId;
+const { application } = require('express');
 
-/* SETUP EXPRESS */
-const app = express();
-
-// VIEW ENGINE
-app.set('view engine', 'hbs');
-
-// STATIC FOLDER
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
-
-// SETUP WAX-ON
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts');
-
-hbs.handlebars.registerHelper('ifEquals', function(arg1, arg2, options){
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
-
-/* ROUTES */
-// GET
-app.get('/', (req, res) => {
+async function main() {
     
-    res.render('index')
-})
 
-app.get('/suggestions', (req, res) => {
-    res.render('suggestion_form')
-})
+    /* SETUP EXPRESS */
+    const app = express();
 
-app.get('/music', (req, res) => {
-    let favorite = 'Coloratura';
-    res.render('music', {
-        'songs': ['My Universe', 'Coloratura', 'Viva la Vida'],
-        'favoriteSong': favorite
-    })
-    // let title = req.params.title;
-    // res.send("Title: " + title)
-})
+    // VIEW ENGINE
+    app.set('view engine', 'hbs');
 
-app.get('/books', (req, res) => {
-    // res.send('Books Route!')
-})
+    // STATIC FOLDER
+    app.use(express.static('public'));
+    app.use(express.urlencoded({extended:false}));
+    app.use(express.json());
+    app.use(cors());
 
-// POST
-app.post('/suggestions', (req, res) => {
-    console.log(req.body);
-    // res.send('suggestion received')
-    let { name, email, title, categoryOptions } = req.body;
-    res.render('display_suggestion', {
-        name,
-        email,
-        title,
-        categoryOptions
+    // SETUP WAX-ON
+    wax.on(hbs.handlebars);
+    wax.setLayoutPath('./views/layouts');
+
+    hbs.handlebars.registerHelper('ifEquals', function(arg1, arg2, options){
+        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
     });
-})
 
-// PUT
-// app.put('/', (req, res) => {``})
+    await MongoUtil.connect(process.env.ATLAS_URI, 'music_coldplay');
 
-// DELETE
-// app.delete('/', (req, res) => {``})
+    /* ROUTES */
+    // GET
+    app.get('/', (req, res) => {
+        
+        res.render('index')
+    })
 
-// PATCH
-// app.patch('/', (req, res) => {``})
+    app.get('/suggestions', (req, res) => {
+        res.render('suggestion_form')
+    })
 
-/* SERVER */
-app.listen(3000, () => {
-    console.log ("Server ready!")
-})
+    app.get('/music', (req, res) => {
+        let favorite = 'Coloratura';
+        res.render('music', {
+            'songs': ['My Universe', 'Coloratura', 'Viva la Vida'],
+            'favoriteSong': favorite
+        })
+        // let title = req.params.title;
+        // res.send("Title: " + title)
+    })
+
+    app.get('/books', (req, res) => {
+        // res.send('Books Route!')
+    })
+
+    // POST
+    app.post('/suggestions', (req, res) => {
+        console.log(req.body);
+        // res.send('suggestion received')
+        let { name, email, title, categoryOptions } = req.body;
+        res.render('display_suggestion', {
+            name,
+            email,
+            title,
+            categoryOptions
+        });
+    })
+
+    // PUT
+    // app.put('/', (req, res) => {``})
+
+    // DELETE
+    // app.delete('/', (req, res) => {``})
+
+    // PATCH
+    // app.patch('/', (req, res) => {``})
+
+    /* SERVER */
+    app.listen(3000, () => {
+        console.log ("Server ready!")
+    })
+}
+main();
