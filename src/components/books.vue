045dd59f6a874ade9a2663207103fa0f3479e5e2
Alexis Dela Torre <alexisndelatorre@gmail.com>
2022-01-25 17:57:40 +0000

    extracted database. initial filtering, successful also. WIP make a simple UI/UX for the web app

--- src/components/books.vue
+++ src/components/books.vue
@@ -1,21 +1,41 @@
 <template>
   <div>
-
+    <div class="container">
       <h1>Books Section</h1>
-
+      <input type="text" v-model="title" />...
+        <ul>
+          <li v-for="(b, index) in filteredBooks" :key="index">{{b.title}}, {{b.author}}</li>
+        </ul>
+    </div>
+      
   </div>
 </template>
 
 <script>
 const BASE_API_URI = "https://silid-aklatan-api.herokuapp.com"
 
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
+  data: function() {
+    return {
+      books: [],
+      title: ''
+    }
+  },
   created: async function () {
     let response = await axios.get(BASE_API_URI + '/books');
-    console.log(response.data);
-  }
+    this.books = response.data;
+    console.log('created')
+  },
+  computed: {
+    filteredBooks : function () {
+      let filtered = this.books.filter((eachBook) => {
+        return eachBook.title.toLowerCase().includes(this.title.toLowerCase())
+      })
+      return filtered;
+    }
+  },
 }
 </script>
 
