2b4828c0fcf6f51b351eeefe2f6d59aed9a9ab3b
alexisnapoles <alexisndelatorre@gmail.com>
2022-02-04 12:10:07 +0800

    re installed yarn

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import 'bulma/css/bulma.min.css'
 
 // this line allows axios to be registered globally to avoid repeating the import from different components
-
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
