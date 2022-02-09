411f3ffdaffdb43e6c8b7b7aeaaee2e6369db124
alexisnapoles <alexisndelatorre@gmail.com>
2022-02-09 11:40:00 +0800

    added bootstrap-vue: so far so good

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <BooksListVue/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BooksListVue from "./components/BooksList.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+   BooksListVue
+}
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
